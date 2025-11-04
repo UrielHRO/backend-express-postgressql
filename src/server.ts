ea56adb71d58941dc0d24328fcd6cc869c114b4a
UrielhRibeiro <uriel.h.ribeiro@gmail.com>
2025-11-04 19:25:02 -0300

    feat: implementação do CORS

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'
 import 'express-async-errors';
 import authRoutes from './routes/authRoutes';
 import noteRoutes from './routes/noteRoutes';
@@ -12,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware para processar JSON
+app.use(cors());
 app.use(express.json());
 
 // Rota de boas-vindas
